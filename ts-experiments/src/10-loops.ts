b4fe2133bfc81a9febd94d52f9b0c0fa27bca51c
Magesh Kuppan <tkmagesh77@gmail.com>
2025-11-27 16:52:31 +0530

    more on loops

--- ts-experiments/src/10-loops.ts
+++ ts-experiments/src/10-loops.ts
@@ -1,3 +1,5 @@
+import { Expect, Equal } from "type-testing";
+
 namespace loops_demo {
 
     type Column = {
@@ -25,7 +27,7 @@ namespace loops_demo {
     const ages = getColumn(users, "age")
     // type of ages => number[]
 
-    declare function getColumn<T extends Table, N extends string>(table : T, columnName : N) : GetColumn<T,N> 
+    // declare function getColumn<T extends Table, N extends string>(table : T, columnName : N) : GetColumn<T,N> 
 
     type t1 = GetColumn<UserTable, "firstname">
     // t1 => string[]
@@ -41,4 +43,49 @@ namespace loops_demo {
             ? Values 
             : GetColumn<Rest, Name>
         : undefined
+
+    function getColumn<T extends Table, N extends string>(table: T, columnName: N): GetColumn<T, N> {
+        const column = table.find(col => col.name === columnName)
+
+        if (column === undefined){
+            return undefined as GetColumn<T,N>
+        }
+
+        return column.values as GetColumn<T,N>
+    }
+
+    const userNames = getColumn(users, "firstname")
+    const formattedNames = userNames.map(n => n.toUpperCase())
+
+    const userAges = getColumn(users, "age")
+    const serniors = userAges.filter(age => age > 50)
+
+}
+
+namespace map_demo {
+    type Names = ToNames<[
+        {id : 1, name : "magesh"},
+        {id : 2, name : "suresh"}
+    ]>
+
+    type GetName<User> = User extends { id: number, name: infer Name } ? Name : "anonymous"
+
+    // Names => ["magesh", "suresh"]
+    type ToNames<List> = List extends [infer First, ...infer Rest]
+        ? [ GetName<First>, ...ToNames<Rest>]
+        : []
+
+    type t1 = Expect<Equal<Names, ["magesh", "suresh"]>>
+}
+
+namespace filter_demo {
+    type Numbers = OnlyNumbers<[1,3,"magesh",true,5]>
+
+    type OnlyNumbers<List> = List extends [infer First, ...infer Rest ] 
+        ? First extends number
+            ? [First, ...OnlyNumbers<Rest>]
+            : OnlyNumbers<Rest>
+        : []
+
+    type t = Expect<Equal<Numbers, [1,3,5]>>
 }
